81898d55cc9d3dde236367f718df1b19c2197555
Your Name <ervthirumal14321@gmail.com>
2025-05-12 14:28:33 +0530

    ff4

--- src/app/Middlewares/GlobalUrlErrorHandler.js
+++ src/app/Middlewares/GlobalUrlErrorHandler.js
@@ -5,7 +5,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const AxiosInstance = axios.create({
     baseURL: BASE_URL,
-    timeout: 10000,
+    timeout: 20000,
     headers: {
         "Content-Type": "application/json"
     }
